11686e8363c4379bf901c8fd1a7ef95d110594b8
Andrewpolewoy <andrewpolewoy@gmail.com>
2020-09-23 12:00:27 +0300

    second commit

---
+++ MySecondProgram.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class MySecondProgram{
+	public static void main(String[] chtougodno){
+		System.out.println("Vvedite imya");
+		Scanner scan = new Scanner(System.in);
+		String name = scan.nextLine();
+
+		System.out.println("Hello !!!!!! " + name);
+	}
+}
+	
