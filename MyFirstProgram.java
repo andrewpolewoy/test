b6effe48ab89abf55f90de92d5b7470580d3557f
Andrewpolewoy <andrewpolewoy@gmail.com>
2020-09-23 11:36:05 +0300

    first commit

---
+++ MyFirstProgram.java
@@ -0,0 +1,9 @@
+public class MyFirstProgram{
+	public static void main(String[] args){
+		for(String arg : args){
+			System.out.println("Привет " + arg);
+		}
+		System.out.println("Hello World");
+	}
+}
+	
